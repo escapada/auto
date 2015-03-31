e6361ace360048384a7cf6157dca6bd4f479b08a
no-body <ivan@abc-design.ru>
2015-03-31 16:53:34 +0300

    blank refinery

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Auto::Application.config.secret_token = '6c2314213ecc3969b7fbff70417a0cf38842c72f3450bde332f4d8351989e25b6490ae748827771aa5450c838d7e00ad796cd9087f6c38bb77115fd524e64226'
